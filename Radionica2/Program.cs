4254b9663f0f19d7b11465c9bb84c4e0c3c2e739
karasicevic <ak20190190@student.fon.bg.ac.rs>
2022-11-02 13:05:44 +0100

    izmena druge grane

--- Radionica2/Program.cs
+++ Radionica2/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dodat jos jedan console log");
+
+var b = Console.Read();
+Console.WriteLine("lalalalal");
