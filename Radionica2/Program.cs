d23325bea4d3b7018d3cda94f826645d627c30fc
karasicevic <ak20190190@student.fon.bg.ac.rs>
2022-11-02 12:55:57 +0100

    dodat conole write

--- Radionica2/Program.cs
+++ Radionica2/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dodat jos jedan console log");
+
+var a = Console.ReadLine();
+Console.WriteLine("dodat");
+
+Console.WriteLine("aaaa");
+
